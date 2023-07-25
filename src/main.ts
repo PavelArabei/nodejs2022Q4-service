8f702e14f43595f76ffdb9c7c411dbc582806f5e
Pavel <Ptashkaaaaaa@gmail.com>
2023-07-25 19:54:27 +0200

    init: init project

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as dotenv from 'dotenv';
+import * as process from 'process';
 
 async function bootstrap() {
+  dotenv.config();
+  const { PORT } = process.env;
   const app = await NestFactory.create(AppModule);
-  await app.listen(4000);
+  await app.listen(PORT || 4000);
+  console.log(PORT);
 }
 bootstrap();
