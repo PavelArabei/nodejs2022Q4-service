2fc3f4f9addac33e2608a97fb7ac60fe0b939600
Pavel <Ptashkaaaaaa@gmail.com>
2023-08-20 17:19:05 +0200

    feat: add logs files are rotated with size, add environment variable to specify max file size.

--- src/logging/logging.service.ts
+++ src/logging/logging.service.ts
@@ -1,42 +1,62 @@
 import { Injectable, Logger } from "@nestjs/common";
-import * as path from "path";
-import * as fs from "fs";
+import { join } from "path";
+import { createWriteStream, readdir, statSync, WriteStream } from "fs";
 import { appRoot } from "@app/main";
+import { LogAndError } from "@app/logging/types/logAndError";
+import * as process from "process";
+
 
 @Injectable()
 export class MyLoggingService {
   private logger: Logger;
+  private logFileStream: WriteStream;
+  private errorFileStream: WriteStream;
+  private readonly maxSize: number;
 
 
   constructor() {
     this.logger = new Logger();
+    const { LOG_FILE_SIZE } = process.env;
+    this.maxSize = +LOG_FILE_SIZE;
+
+    this.rotateFile(this.logFileStream, "log");
+    this.rotateFile(this.errorFileStream, "err");
   }
 
   log(message: string) {
     const color = this.getColorCode("blue");
     this.logger.log(`${color}${message}`);
-    this.writeToFile(message, this.logPath);
+
+
+    this.logFileStream.write(`${message}\n`);
+    this.fileSize("log");
+
   }
 
   error(message: string, trace?: string, context?: string) {
     const color = this.getColorCode("red");
     this.logger.error(`${color}${message}`, `${color}${trace}`, context);
-    this.writeToFile(`${message} ${trace}`, this.errPath);
+
+    this.errorFileStream.write(`${message}\n ${trace}`);
+    this.fileSize("err");
   }
 
   warn(message: string, context?: string) {
     this.logger.warn(message, context);
-    this.writeToFile(message, this.logPath);
+    this.logFileStream.write(`${message}\n`);
+    this.fileSize("log");
   }
 
   debug(message: string, context?: string) {
     this.logger.debug(message, context);
-    this.writeToFile(message, this.logPath);
+    this.logFileStream.write(`${message}\n`);
+    this.fileSize("log");
   }
 
   verbose(message: string, context?: string) {
     this.logger.verbose(message, context);
-    this.writeToFile(message, this.logPath);
+    this.logFileStream.write(`${message}\n`);
+    this.fileSize("log");
   }
 
   private getColorCode(color: string): string {
@@ -52,28 +72,67 @@ export class MyLoggingService {
   }
 
   private get folderPath() {
-    return path.join(appRoot, "..", "logs");
+    return join(appRoot, "..", "logs");
   }
 
   private get logPath() {
-    return path.join(this.folderPath, "app.log");
+    return join(this.folderPath, "app.log");
   }
 
   private get errPath() {
-    return path.join(this.folderPath, "err.log");
+    return join(this.folderPath, "err.log");
   }
 
 
-  private async writeToFile(message, pathToFile) {
-    const errorMessage = `${new Date().toISOString()} - ${message}\n`;
-    const writeStream = fs.createWriteStream(pathToFile, { flags: "a" });
+  private fileSize(type: LogAndError) {
+    const stats = type === "log" ? statSync(this.logFileStream.path) : statSync(this.errorFileStream.path);
+    
+    if (stats.size >= this.maxSize) {
+      const stream = type === "log" ? this.logFileStream : this.errorFileStream;
+      this.rotateFile(stream, type);
+    }
+  }
 
-    writeStream.write(errorMessage);
-    writeStream.end();
+  async getAvailableToWriteFile(prefix: LogAndError) {
+    const filePath = await new Promise<string | undefined>((res) => {
+      readdir(this.folderPath, (err, files) => {
+        if (err) res(undefined);
+        if (files.length === 0) res(undefined);
 
-    writeStream.on("error", (err) => {
-      console.error("Error writing to file:", err);
+        const logFiles = files.filter((file) => {
+          const filePath = join(this.folderPath, file);
+          const stats = statSync(filePath);
+          const { size } = stats;
+          console.log(this.maxSize);
+          return file.startsWith(prefix) && file.endsWith(".log") && size < this.maxSize;
+        });
+
+        if (logFiles[0]) res(logFiles[0]);
+        else res(undefined);
+      });
     });
+    console.log(filePath);
+    return filePath;
+  }
+
+  private async rotateFile(stream: WriteStream, type: LogAndError) {
+    if (stream) stream.close();
+
+    let fileName: string;
+    if (type === "log") {
+      const oldFileName = await this.getAvailableToWriteFile("log");
+      if (oldFileName) fileName = oldFileName;
+      else fileName = `${type}-${+new Date()}.log`;
+    } else {
+      const oldFileName = await this.getAvailableToWriteFile("err");
+      if (oldFileName) fileName = oldFileName;
+      else fileName = `${type}-${+new Date()}.log`;
+    }
+
+
+    const newStream = createWriteStream(join(this.folderPath, fileName), { flags: "a" });
+    if (type === "log") this.logFileStream = newStream;
+    else this.errorFileStream = newStream;
   }
 
 
